aad72e09a26d952cb3638c174a8b0e5173f2f8f2
Mangi Lal <mangilal1036@gmail.com>
2023-12-10 21:31:18 +0530

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,28 @@
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// Pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import ArticlesList from "./pages/ArticlesList";
+import Article from "./pages/Article";
+import NotFound from "./pages/NotFound";
+
+// Components
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="bg-red-500 text-center text-white">
-      Hello
-    </div>
+    <Router>
+      <Navbar />
+      <div className='max-w-screen-md mx-auto pt-20'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/articles-list' element={<ArticlesList />} />
+          <Route path='/article/:name' element={<Article />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
